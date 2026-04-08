5855d298af244f91b31c6be093fc61301c433181
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2026-04-08 15:09:13 +0530

    add RootLayout component for main application structure and styling

---
+++ client/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../app/global.css";
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kineq",
+  description:
+    "Organize and track your Asian dramas and anime watchlist in your own creative way! Take full control over your watchlist with Kineq.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  
+ 
+  return (
+    <html lang="en" className="scroll-smooth hydrated">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black `}
+      >
+        <Header />
+        <main className="mx-auto min-h-screen bg-white px-4">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
