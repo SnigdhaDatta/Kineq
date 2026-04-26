08724bffc365ada6f5a9cca8f136ae9a0fdd22a9
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2026-04-26 19:22:02 +0530

    feat: implement email verification and OTP functionality in user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,10 @@ const UserRouter = Router();
 const {createAccessToken, createRefreshToken} = require("../services/authentication");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
+const sendOnboardingMail = require("../services/mail").sendOnboardingMail;
+const redisClient = require("../services/client");
+const sendEmailVerificationMail = require("../services/mail").sendEmailVerificationMail;
+
 require('dotenv').config();
 
 UserRouter.post("/signup", async (req,res)=>{
@@ -13,7 +17,7 @@ UserRouter.post("/signup", async (req,res)=>{
         if(existingUser){
             return res.status(409).json({error: 'No need to Signup as user with this email already exists. Please login instead.'});
         }
-        if(!fullName || !email || !password || typeof isVerified === 'undefined'){
+        if(!fullName || !email || !password || !isVerified){
             return res.status(400).json({error: 'Please Provide all the required Fields'});
         }
         const user = await User.create({fullName, email, password, isVerified});
@@ -33,8 +37,8 @@ UserRouter.post("/signup", async (req,res)=>{
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // Set the cookie to expire in 7 days from now
         });
-
-        return res.status(201).json({message: 'Signup successful'});
+        await sendOnboardingMail(email, fullName, "Welcome to Kineq !");
+        return res.status(201).json({message: 'Signup successful'})
         
     } catch (error) {
         res.status(500).json({error: error.message});
@@ -80,6 +84,38 @@ UserRouter.post("/login", async (req,res)=>{
         res.status(500).json({error: "Internal Server Error"});
     }
 })
+
+UserRouter.post("/sendOtp", async(req,res)=>{
+    const {email} = req.body;
+    if(!email){
+        return res.status(400).json({error:'Email is required'});
+    }
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();//otp will be a 6 digit random number
+    await redisClient.set(`otp:${email}`, otp, "EX", 120); //otp will expire in 2 minute
+    await sendEmailVerificationMail(email, otp);
+    return res.status(200).json({message:'OTP sent successfully in your mail, if not found in Inbox, please check your Spam folder.', otp});
+})
+
+UserRouter.post("/verifyEmail", async(req,res)=>{
+    const {email, otp} = req.body;  
+    if(!email || !otp){
+        return res.status(400).json({error:'Email and OTP are required'});
+    }
+    try {
+        const storedOtp = await redisClient.get(`otp:${email}`);
+        if(!storedOtp){
+            return res.status(400).json({error:'OTP expired or not found. Please request a new OTP.'});
+        }
+        if(storedOtp !== otp){
+            return res.status(400).json({error:'Invalid OTP. Please try again.'});
+        }
+        await redisClient.del(`otp:${email}`);
+        return res.status(200).json({message:'Email verified successfully!'});
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+})
+
 
 
 UserRouter.post("/logout", async (req,res)=>{
